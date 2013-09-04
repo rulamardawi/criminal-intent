687df2025af99bf8814c510f8a6da3d71ae15c12
Jim Kirkbride <jkirkbride@pillartechnology.com>
2013-09-03 22:51:00 -0400

    Added list view.  Added logic to populate the list view via an ArrayAdapter.

--- src/com/jameskbride/criminalIntent/CrimeListFragment.java
+++ src/com/jameskbride/criminalIntent/CrimeListFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 public class CrimeListFragment extends ListFragment {
@@ -20,13 +24,55 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crimes_title);
         crimes = CrimeLab.getInstance(getActivity()).getCrimes();
 
-        ArrayAdapter<Crime> crimeArrayAdapter = new ArrayAdapter<Crime>(getActivity(), android.R.layout.simple_list_item_1, crimes);
+        CrimeAdapter crimeArrayAdapter = new CrimeAdapter(crimes);
         setListAdapter(crimeArrayAdapter);
     }
 
     @Override
     public void onListItemClick(ListView listView, View view, int position, long id) {
-        Crime crime = (Crime)(getListAdapter()).getItem(position);
+        Crime crime = ((CrimeAdapter)getListAdapter()).getItem(position);
         Log.d(TAG, crime.getTitle() + " was clicked");
+    }
+
+    private class CrimeAdapter extends ArrayAdapter<Crime> {
+
+        public CrimeAdapter(List<Crime> crimes) {
+            super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = createView();
+            }
+
+            Crime crime = getItem(position);
+
+            populateTitle(convertView, crime);
+            populateDate(convertView, crime);
+            populateSolved(convertView, crime);
+
+            return convertView;
+        }
+
+        private void populateSolved(View convertView, Crime crime) {
+            CheckBox solvedCheckBox = (CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(crime.isSolved());
+        }
+
+        private void populateDate(View convertView, Crime crime) {
+            TextView dateTextView = (TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+            dateTextView.setText(dateFormatter.format(crime.getDiscoveredOn()));
+        }
+
+        private void populateTitle(View convertView, Crime crime) {
+            TextView titleTextView = (TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(crime.getTitle());
+        }
+
+        private View createView() {
+            return getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+        }
     }
 }
