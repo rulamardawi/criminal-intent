e33f4d5648bc1eaf853e43f2e39ca2dd1f2b5189
Jim Kirkbride <jkirkbride@pillartechnology.com>
2013-09-24 10:37:14 -0400

    Adding ActionBar support for froyo.

--- src/com/jameskbride/criminalIntent/SingleFragmentActivity.java
+++ src/com/jameskbride/criminalIntent/SingleFragmentActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends ActionBarActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
