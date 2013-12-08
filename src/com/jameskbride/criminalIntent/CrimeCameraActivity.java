7386a92e59f61a38066b6ee2329c549f4edd8b47
Jim Kirkbride <james.kirkbride@gmail.com>
2013-12-08 11:33:09 -0500

    Adding logic to disable the title and set camera view to fullscreen.

--- src/com/jameskbride/criminalIntent/CrimeCameraActivity.java
+++ src/com/jameskbride/criminalIntent/CrimeCameraActivity.java
@@ -1,11 +1,21 @@
 package com.jameskbride.criminalIntent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
     }
 }
